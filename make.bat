3bd2c5a97a6dfb8c3165343d43ea9abdb0152750
Nick Gordon <devnull@localhost>
2013-09-30 15:52:10 -0700

    Found a version of twitterizer recompiled to support the twitter 1.1 api, but even THAT project is abandoned now... need to look into a future solution. https://bitbucket.org/szrsharp/twitterizer-remaked-for-api-1.1
    Had to update the project to .NET 4.0, which involved changing some of the build parameters for ILMerge and other things

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild.exe .\src\twt.csproj /property:Configuration=Release
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\src\twt.csproj /property:Configuration=Release
